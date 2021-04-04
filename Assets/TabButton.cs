463b4f6fee7b26f3bfe97d7be03ba0b0fb6c65e8
Varun Sharma <varunk22110@gmail.com>
2021-04-04 19:26:05 +0530

    Implemeted Scroll Menu demo and TabSystem demo.

---
+++ Assets/TabButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class TabButton : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
+{
+    public TabGroup tabGroup;
+
+    public void OnPointerClick(PointerEventData eventData) {
+        tabGroup.OnTabSelected(this);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        tabGroup.OnTabEnter(this);
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        tabGroup.OnTabExit(this);
+    }
+
+    void Start()
+    {
+        tabGroup.Subscribe(this);
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/TabGroup.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabGroup : MonoBehaviour
+{
+    public List<TabButton> tabButtons;
+    public List<GameObject> objectsToSwap;
+
+    public void Subscribe(TabButton button) {
+        if(tabButtons == null) {
+            tabButtons = new List<TabButton>();
+        }
+
+        tabButtons.Add(button);
+    }
+
+    public void OnTabEnter(TabButton button) {
+
+    }
+
+    public void OnTabExit(TabButton button) {
+
+    }
+    
+    public void OnTabSelected(TabButton button) {
+        int index = button.transform.GetSiblingIndex();
+        for (int i = 0; i < objectsToSwap.Count; i++) {
+            if (i == index) {
+                objectsToSwap[i].SetActive(true);
+            } else {
+                objectsToSwap[i].SetActive(false);
+            }
+        
+        }
+    }
+}
